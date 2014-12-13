fe5142b989f6a46221e8bf5d6593c66885134a15
Harang Ju <wngkfkd94@gmail.com>
2014-12-13 16:47:48 -0500

    Added createNetwork function with just connectivity (no randomized weights)

--- Network/Network.hs
+++ Network/Network.hs
@@ -6,10 +6,21 @@ module Network.Network
 import Network.Neuron
 import Network.Layer
 import Network.Trainer
+import Linear
 
 -- Networks are constructed front to back. Start by adding an input layer,
 -- then each hidden layer, and finally an output layer.
-data Network a = Network [LayerDefinition a]
+--data Network a = Network [LayerDefinition a]
+
+type Network a = [(Matrix a, Matrix a)]
+
+createNetwork :: (Floating a) => [LayerDefinition a] -> Network a
+createNetwork [] = []
+createNetwork (layerDef : []) = []
+createNetwork (layerDef : (layerDef' : otherLayerDefs)) =
+  ( (connect layerDef) (neuronCount layerDef) (neuronCount layerDef'),
+    (connect layerDef) (neuronCount layerDef) (neuronCount layerDef'))
+  : createNetwork (layerDef' : otherLayerDefs)
 
 addLayerDefinition :: (Floating a) => LayerDefinition a -> [LayerDefinition a] -> [LayerDefinition a]
 addLayerDefinition layer layers = (layers ++ [layer])
