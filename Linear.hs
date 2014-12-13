5d7041939f6a55f78321dfc2dffda833fa9549ae
Harang Ju <wngkfkd94@gmail.com>
2014-12-13 17:10:36 -0500

    Refactored reshape

--- Linear.hs
+++ Linear.hs
@@ -21,9 +21,9 @@ hadamard :: (Num a) => Matrix a -> Matrix a -> Matrix a
 hadamard m n = zipWith (zipWith (*)) m n
 
 -- parameters
---   i is the number of rows of the resulting matrix
+--   j is the number of columns of the resulting matrix
 -- returns
---   
+--   i by j matrix where i is length of list / j
 reshape :: (Num a) => Int -> [a] -> Matrix a
-reshape i [] = []
-reshape i list = [(take i list)] ++ reshape i (drop i list)
+reshape j [] = []
+reshape j list = [(take i list)] ++ reshape i (drop i list)
