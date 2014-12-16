853c826cabfce05292689967edb68e19f721a84d
Brent Baumgartner <brent@brentbaum.com>
2014-12-16 01:03:40 -0500

    Server so far.

---
+++ server2.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE OverloadedStrings, TypeFamilies, DeriveDataTypeable, TemplateHaskell #-}
+
+module Main where
+
+import Web.Scotty
+
+import Control.Monad.IO.Class
+import Control.Applicative ((<$>), (<*>), empty)
+        
+import Data.Default (def)
+import Data.Typeable
+import Data.Aeson
+import qualified Data.Map as Map
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Text.Lazy as T
+import Data.Text.Lazy.Encoding
+        
+
+-- Network
+import Network.Wai.Handler.Warp (settingsPort)
+import Network.HTTP.Types  (status404, status200)
+
+------------------------------------------------------------------------------
+
+config :: Options
+config = def { verbose = 0
+           , settings = (settings def) { settingsPort = 4000 } }
+
+data Coord = Coord { x :: Double, y :: Double }
+             deriving (Show)
+
+instance FromJSON Coord where
+    parseJSON (Object v) = Coord <$>
+                           v .: "x" <*>
+                             v .: "y"
+    parseJSON _          = empty
+
+------------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+    putStrLn "Starting HTTP Server."
+    let req = decode "{\"x\":3.0,\"y\":-1.0}" :: Maybe Coord
+    print req
+    scottyOpts config $ do
+        post "/create/" $
+             do
+               wb <- body
+               text $ decodeASCII wb
+
+
+------------------------------------------------------------------------------
