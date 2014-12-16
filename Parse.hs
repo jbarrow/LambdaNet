434c2c7ef77f5399cf1d6be92accb79743cb5d6c
Alex Thomas <apaulthomas93@gmail.com>
2014-12-16 12:06:26 -0500

    Started toNetworkDefinition

--- Parse.hs
+++ Parse.hs
@@ -33,6 +33,12 @@ data NetworkParseDefinition = NetworkParseDefinition { layers :: [LayerParseDefi
 instance FromJSON NetworkParseDefinition
 instance ToJSON NetworkParseDefinition
 
+--toNetworkDefinition :: NetworkParseDefinition -> Network a
+--toNetworkDefinition d {layers=layerDefs, init=initDistribution}
+--    | init == "normal" = createNetwork  
+--    | init == "uniform" = 
+--    | otherwise = 
+
 data TrainingParseDefinition = TrainingParseDefinition { trainingdata :: [(Matrix Float, Matrix Float)]
                                                        , nw :: [Matrix Float]
                                          } deriving (Generic, Show)
