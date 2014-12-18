d9315fd64999af9081687dac17224c7b0fa26136
Joseph Barrow <joseph.d.barrow@gmail.com>
2014-12-17 22:20:32 -0500

    Fixed comments in Neuron.hs.

--- Network/Neuron.hs
+++ Network/Neuron.hs
@@ -19,9 +19,9 @@ module Network.Neuron
 , input
 ) where
 
--- Using this structure allows users of the library to create their own
--- neurons by creating two functions - an activation function and its
--- derivative - and packaging them up into a neuron type.
+-- | Using this structure allows users of the library to create their own
+--   neurons by creating two functions - an activation function and its
+--   derivative - and packaging them up into a neuron type.
 data Neuron a = Neuron { activation :: (ActivationFunction a)
                        , activation' :: (ActivationFunction' a)
                        }
@@ -29,16 +29,16 @@ data Neuron a = Neuron { activation :: (ActivationFunction a)
 type ActivationFunction a = a -> a
 type ActivationFunction' a = a -> a
 
--- Evaluate the activation of a neuron by pattern matching it on
--- the constructor.
+-- | Evaluate the activation of a neuron by pattern matching it on
+--   the constructor.
 evaluate :: (Floating a) => Neuron a -> [a] -> a
 evaluate (Neuron f f') = f . sum
 
--- Evaluate the derivative of a neuron, given a floating point vector
+-- | Evaluate the derivative of a neuron, given a floating point vector
 evaluate' :: (Floating a) => Neuron a -> [a] -> a
 evaluate' (Neuron f f') = f' . sum
 
--- Our provided neuron types: sigmoid, tanh, reclu
+-- | Our provided neuron types: sigmoid, tanh, reclu
 sigmoidNeuron :: (Floating a) => Neuron a
 sigmoidNeuron = Neuron sigmoid sigmoid'
 
@@ -48,35 +48,34 @@ tanhNeuron = Neuron tanh tanh'
 recluNeuron :: (Floating a) => Neuron a
 recluNeuron = Neuron reclu reclu'
 
--- The sigmoid activation function, a standard activation function defined
--- on the range (0, 1).
+-- | The sigmoid activation function, a standard activation function defined
+--   on the range (0, 1).
 sigmoid :: (Floating a) => a -> a
 sigmoid t = 1 / (1 + exp (-1 * t))
 
--- The derivative of the sigmoid function conveniently can be computed in terms
--- of the sigmoid function.
+-- | The derivative of the sigmoid function conveniently can be computed in terms
+--   of the sigmoid function.
 sigmoid' :: (Floating a) => a -> a
 sigmoid' t = s * (1 - s)
               where s = sigmoid t
 
--- The hyperbolic tangent activation function is provided in Prelude. Here
--- we provide the derivative
--- As with the sigmoid function, the derivative of tanh can be computed in
--- terms of tanh.
+-- | The hyperbolic tangent activation function is provided in Prelude. Here
+--   we provide the derivative. As with the sigmoid function, the derivative
+--   of tanh can be computed in terms of tanh.
 tanh' :: (Floating a) => a -> a
 tanh' t = 1 - s ^ 2
                where s = tanh t
 
--- The rectified linear activation function. This is a more "biologically
--- accurate" activation function that still retains differentiability.
+-- | The rectified linear activation function. This is a more "biologically
+--   accurate" activation function that still retains differentiability.
 reclu :: (Floating a) => a -> a
 reclu t = log (1 + exp t)
 
--- The derivative of the rectified linear activation function is just the
--- sigmoid.
+-- | The derivative of the rectified linear activation function is just the
+--   sigmoid.
 reclu' :: (Floating a) => a -> a
 reclu' t = sigmoid t
 
--- Input neurons
+-- | Input neurons
 input :: (Floating a) => a -> a
 input t = t
