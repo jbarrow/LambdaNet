85e8c8bc81325a6f43fbc7210e2ffb0aa9d25990
Joseph Barrow <joseph.d.barrow@gmail.com>
2014-12-18 17:38:23 -0500

    Removed more cruft.

--- XOR.hs
+++
@@ -1,4 +0,0 @@
-let l = Layer [[-11.62, 12.88], [10.99, -13.13]] [[-6.06, -7.19]] sigmoidNeuron
-let l' = Layer [[13.34], [13.13]] [[-6.56]] sigmoidNeuron
-let n = Network [l, l']
-predict [[0, 1]] n
