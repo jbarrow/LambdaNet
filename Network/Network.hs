11d9dcaeb8893b3f0a630327bce76e7567d20c3d
Joseph Barrow <joseph.d.barrow@gmail.com>
2015-01-01 16:34:56 -0500

    Added network as a Monoid to prepare for selection functions.

--- Network/Network.hs
+++ Network/Network.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts,
+             UndecidableInstances #-}
 
 module Network.Network
 ( Network(..)
@@ -6,6 +7,9 @@ module Network.Network
 
 , createNetwork
 , loadNetwork
+, emptyNetwork
+, isEmptyNetwork
+, addNetworks
 , predict
 , apply
 , saveNetwork
@@ -17,11 +21,19 @@ import System.Random
 import Numeric.LinearAlgebra
 import qualified Data.ByteString.Lazy as B
 import System.IO
+import Data.Monoid (Monoid(..))
 import Data.Binary (encode, decode, Binary(..))
 
 -- | Networks are constructed front to back. Start by adding an input layer,
 --   then each hidden layer, and finally an output layer.
 data Network a = Network { layers :: [Layer a] }
+
+-- | We gain the ability to combine two networks of the same proportions
+--   by abstracting a network as a monoid. This is useful in backpropagation
+--   for batch training
+instance (Product a, Container Vector a, Floating (Vector a)) => Monoid (Network a) where
+  mempty = emptyNetwork
+  mappend = addNetworks
 
 -- | A tuple of (input, expected output)
 type TrainingData a = (Vector a, Vector a)
@@ -37,6 +49,26 @@ createNetwork t g (layerDef : layerDef' : otherLayerDefs) =
   Network (layer : layers restOfNetwork)
   where layer = createLayer t g layerDef layerDef'
         restOfNetwork = createNetwork t g (layerDef' : otherLayerDefs)
+
+-- | Our Unit, an empty network with no layers
+emptyNetwork :: (Product a)
+  => Network a
+emptyNetwork = Network []
+
+-- | A boolean to check if the network is the unit network or not
+isEmptyNetwork :: (Product a)
+  => Network a -> Bool
+isEmptyNetwork n = length (layers n) == 0
+
+-- | A function to combine two networks
+addNetworks :: (Floating (Vector a), Container Vector a, Product a)
+  => Network a -> Network a -> Network a
+addNetworks n1 n2 = if isEmptyNetwork n1 then n2 else
+  if isEmptyNetwork n2 then n1 else
+    Network $ zipWith combineLayers (layers n1) (layers n2)
+  where combineLayers l1 l2 =
+          Layer ((weightMatrix l1) + (weightMatrix l2))
+          ((biasVector l1) + (biasVector l2)) (neuron l1)
 
 -- | Predict folds over each layer of the network using the input vector as the
 --   first value of the accumulator. It operates on whatever network you pass in.
