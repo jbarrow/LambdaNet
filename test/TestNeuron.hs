51f7cadf4b363f897012bd246f8a04bdd5d44a6d
Joseph Barrow <joseph.d.barrow@gmail.com>
2015-02-11 16:30:11 -0500

    Basic tests for other neuron types.

--- test/TestNeuron.hs
+++ test/TestNeuron.hs
@@ -19,3 +19,21 @@ testNeuron = hspec $ do
       let n = SigmoidNeuron
       E.evaluate (evaluate n (fromList [1, 0]) (fromList [1]))
         `shouldThrow` anyException
+
+  describe "Tanh Neuron" $ do
+    it "should be an instance of Show" $ do
+      (typeOf $ show TanhNeuron) `shouldBe` typeRep ["Char"]
+
+    it "should throw an exception with invalid inputs" $ do
+      let n = TanhNeuron
+      E.evaluate (evaluate n (fromList [1, 0]) (fromList [1]))
+        `shouldThrow` anyException
+
+  describe "Rectified Linear Neuron" $ do
+    it "should be an instance of Show" $ do
+      (typeOf $ show RecluNeuron) `shouldBe` typeRep ["Char"]
+
+    it "should throw an exception with invalid inputs" $ do
+      let n = RecluNeuron
+      E.evaluate (evaluate n (fromList [1, 0]) (fromList [1]))
+        `shouldThrow` anyException
